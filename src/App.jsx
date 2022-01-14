46c56a89a8914a7407c9cd7f4836730ccd250d32
zami tulande <hulmartulande@gmail.com>
2022-01-14 08:33:34 -0500

    useEffect para no perder carga de datos

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,15 @@ function App() {
  const[pacientes, setPacientes] = useState([])
  const[paciente, setPaciente]=useState({})
 
+ useEffect(()=> {
+    const obtenerLS = ()=>{
+      const pacientesLS = JSON.parse(localStorage.getItem('pacientes')) ?? []
+
+      setPacientes(pacientesLS)
+    }
+    obtenerLS()
+   },[])
+
  useEffect(()=>{
     localStorage.setItem('pacientes', JSON.stringify(pacientes))
  },[pacientes])
